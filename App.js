39fa3c66fa5eda7eca690e4af2ae8eda9c0994dd
Sandul Renuja <sandulrenuja@gmail.com>
2021-09-21 15:09:23 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { NativeRouter, Route, Link } from "react-router-native";
+import Login from "./src/Login";
+import Signup from "./src/Signup";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeRouter>
+      <View style={styles.container}>
+        <Route exact path="/" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </View>
+    </NativeRouter>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 25,
+    padding: 10,
   },
 });
+
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
+import React from "react";
+import "react-native-gesture-handler";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+import Login from "./src/Login";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#405DE6",
+    accent: "#405DE6",
+  },
+};
+
+const Main = () => {
+  return (
+    <PaperProvider theme={theme}>
+      <Login />
+    </PaperProvider>
+  );
+};
+
+export default Main;
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Main);
